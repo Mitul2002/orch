51b7d02fb91c7d4018f8713374add45931b63591
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-10-08 18:46:07 +0530

    fix

--- api.py
+++ api.py
@@ -107,4 +107,4 @@ nest_asyncio.apply()
 
 # Now run the FastAPI app using uvicorn within the notebook
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
